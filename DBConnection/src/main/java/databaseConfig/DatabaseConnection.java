4015a76f3c90b209fb56b84abd695717b61d029a
Ahmad Sabateen <ahmad2sabateen@gmail.com>
2021-09-07 17:51:58 +0300

    made db ip localhost

--- DBConnection/src/main/java/databaseConfig/DatabaseConnection.java
+++ DBConnection/src/main/java/databaseConfig/DatabaseConnection.java
@@ -11,7 +11,7 @@ import java.net.Socket;
 
 public class DatabaseConnection {
     private static final int PORT = 80;
-    private static final String IP = "db-service-entrypoint";
+    private static final String IP = "localhost";
     ObjectMapper objectMapper;
     private Socket clientSocket;
     private PrintWriter out;
